45c0fe91a32be760662abb694bc81b7e68dbaad7
swj369451 <swj369451@163.com>
2019-05-12 11:00:23 +0800

    init

---
+++ src/components/user/list.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-row class="warp">
+    <el-col :span="24" class="warp-breadcrum">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }"><b>首页</b></el-breadcrumb-item>
+        <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+      </el-breadcrumb>
+    </el-col>
+
+    <el-col :span="24" class="warp-main" v-loading="loading" element-loading-text="拼命加载中">
+        <!--工具条-->
+        <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+          <el-form :inline="true" :model="filters">
+            <el-form-item>
+              <el-input v-model="filters.name" placeholder="用户名/姓名/昵称" style="min-width: 240px;" @keyup.enter.native="handleSearch"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="handleSearch">查询</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+
+      <!--列表-->
+      <el-table :data="users" highlight-current-row style="width: 100%;">
+        <el-table-column type="index" width="60">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="120" sortable>
+        </el-table-column>
+        <el-table-column prop="nickname" label="昵称" width="120" sortable>
+        </el-table-column>
+        <el-table-column prop="sex" label="性别" width="100" :formatter="formatSex" sortable>
+        </el-table-column>
+        <el-table-column prop="email" label="邮箱" min-width="160" sortable>
+        </el-table-column>
+        <el-table-column prop="addr" label="地址" sortable>
+        </el-table-column>
+      </el-table>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  import API from '../../api/api_user';
+
+  export default {
+    data() {
+      return {
+        filters: {
+          name: ''
+        },
+        users: [
+          {
+            name: '张三',
+            nickname: '小张',
+            sex: '男',
+            email: '1111@qq.com',
+            addr: '厦门'
+          }
+        ],
+        total: 0,
+        page: 1,
+        limit: 10,
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/user/profile.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-row class="warp">
+    <el-col :span="24" class="warp-breadcrum" :loading="loading">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }"><b>首页</b></el-breadcrumb-item>
+        <el-breadcrumb-item>设置</el-breadcrumb-item>
+        <el-breadcrumb-item>个人信息</el-breadcrumb-item>
+      </el-breadcrumb>
+    </el-col>
+
+    <el-col :span="24" class="warp-main">
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="账号">
+          <el-input v-model="form.useranme" disabled></el-input>
+        </el-form-item>
+        <el-form-item prop="nickname" label="昵称">
+          <el-input v-model="form.nickname"></el-input>
+        </el-form-item>
+        <el-form-item prop="name" label="姓名">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item prop="email" label="邮箱">
+          <el-input v-model="form.email"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">修改并保存</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  import API from '../../api/api_user';
+  import {bus} from '../../bus.js'
+
+  export default {
+    data() {
+      return {
+        loading: false,
+        form: {
+          useranme: '',
+          nickname: '',
+          name: '',
+          email: ''
+        },
+        rules: {
+          nickname: [
+            {required: true, message: '请输入昵称', trigger: 'blur'}
+          ],
+          email: [
+            {required: true, message: '请输入邮箱', trigger: 'blur'},
+            {type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change'}
+          ]
+        },
+      }
+    }
+  }
+</script>
